4c657d7873732f21e5f6c1fbb7a6a99a3428eb68
san-gabriels <m.sandugabriel@gmail.com>
2026-04-28 15:51:27 +0000

    typescript error solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "@/app/globals.css";
+import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { SmoothScroll } from "@/components/SmoothScroll";
 
