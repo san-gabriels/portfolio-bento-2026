374f4bef8f4dbdb5cc551f839850055bdee2387d
san-gabriels <m.sandugabriel@gmail.com>
2026-04-28 14:22:50 +0000

    error solving

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/app/globals.css";
 import { Navbar } from "@/components/Navbar";
 import { SmoothScroll } from "@/components/SmoothScroll";
 
